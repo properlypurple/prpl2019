b6a2f13f01ef02f05260b519d685ebe28fac38d0
Properlypurple <git@properlypurple.com>
2018-12-09 11:57:22 +0530

    remove featured image from archive and blog

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,15 +12,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			if ( has_post_thumbnail() ) :
-				the_post_thumbnail();
-			endif;
-		endif;
-
+		the_title( '<h1 class="entry-title">', '</h1>' );
+		
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php gutenbergtheme_posted_on(); ?>
